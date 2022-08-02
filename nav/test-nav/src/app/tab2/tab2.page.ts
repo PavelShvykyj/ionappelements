37d78686581e2e394af8c3e2670784d5d4250076
Швыдкий Павел <paveldv_next@outlook.com>
2022-08-02 15:43:32 +0300

    form basic

--- nav/test-nav/src/app/tab2/tab2.page.ts
+++ nav/test-nav/src/app/tab2/tab2.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  @ViewChild('emailcontrol', { static: false }) emailIonInput: IonInput;
+  form: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      passwordconfirm: ['', Validators.required],
+    });
+  }
 
+  public get emailControl(): AbstractControl {
+    const control = this.form.get('email');
+    return control;
+  }
+
+  ngOnInit(): void {}
+
+  ionViewDidEnter() {
+    this.emailIonInput.setFocus();
+  }
+
+  onInputFocus(focusEvent) {
+    focusEvent.target.getInputElement().then((el) => {
+      el.select();
+    });
+  }
 }
